2c7811e0e5d60ca1adc7b1f576e197ed2511a352
DoYeon Nami Kim <doyeon.kim@mail.mcgill.ca>
2017-05-22 15:58:38 -0400

    sjfesf

--- js/controllers/TechController.js
+++ js/controllers/TechController.js
@@ -1,5 +1,7 @@
 angular.module('namiworld')
 .controller('TechController', function($scope) {
+
+	
 	// Look into Showdown (md to html)
 	// First view will bring up a table of images with overlay texts of the topics for each of the images
 	// appearing from top to bottom, like on oceana.im, fade in and bring up more as you scroll down.
